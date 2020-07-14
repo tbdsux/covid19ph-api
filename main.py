50ef018a8ed55131d30a9fd0e9b7fa4a87958aa2
TheBoringDude <euhsojlogro112002@gmail.com>
2020-07-15 01:49:09 +0800

    moved crawler initiation to function

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ class DataModel(str, Enum):
     total = "all"
 
 
-crawler = Crawler()
-data = crawler.get_all()
+# function
+def get_data():
+    crawler = Crawler()
+    return crawler.get_all()
 
 
 @app.get("/")
@@ -45,56 +47,56 @@ async def read_case(case_name: str):
     if case_name == DataModel.confirmed:
         return {
             "Info": "PH CoVid-19 Data",
-            "Confirmed_Cases": {"total": data["confirmed"]},
+            "Confirmed_Cases": {"total": get_data()["confirmed"]},
             "current_time": datetime.now(),
         }
 
     elif case_name == DataModel.active:
         return {
             "Info": "PH CoVid-19 Data",
-            "Active_Cases": {"total": data["active"]},
+            "Active_Cases": {"total": get_data()["active"]},
             "current_time": datetime.now(),
         }
 
     elif case_name == DataModel.suspected:
         return {
             "Info": "PH CoVid-19 Data",
-            "Suspected_Cases": {"total": data["suspected"]},
+            "Suspected_Cases": {"total": get_data()["suspected"]},
             "current_time": datetime.now(),
         }
 
     elif case_name == DataModel.severe:
         return {
             "Info": "PH CoVid-19 Data",
-            "Severe_Cases": {"total": data["severe"]},
+            "Severe_Cases": {"total": get_data()["severe"]},
             "current_time": datetime.now(),
         }
 
     elif case_name == DataModel.critical:
         return {
             "Info": "PH CoVid-19 Data",
-            "Critical_Cases": {"total": data["critical"]},
+            "Critical_Cases": {"total": get_data()["critical"]},
             "current_time": datetime.now(),
         }
 
     elif case_name == DataModel.recovered:
         return {
             "Info": "PH CoVid-19 Data",
-            "Recovered_Cases": {"total": data["recovered"]},
+            "Recovered_Cases": {"total": get_data()["recovered"]},
             "current_time": datetime.now(),
         }
 
     elif case_name == DataModel.deaths:
         return {
             "Info": "PH CoVid-19 Data",
-            "Deaths_Cases": {"total": data["deaths"]},
+            "Deaths_Cases": {"total": get_data()["deaths"]},
             "current_time": datetime.now(),
         }
 
     elif case_name == DataModel.fatality_rate:
         return {
             "Info": "PH CoVid-19 Data",
-            "Fatality_Rate": {"total": data["fatality_rate"]},
+            "Fatality_Rate": {"total": get_data()["fatality_rate"]},
             "current_time": datetime.now(),
         }
 
